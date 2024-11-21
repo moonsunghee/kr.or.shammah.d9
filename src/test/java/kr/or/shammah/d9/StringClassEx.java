af32876e0acdc3dc15940cf1a9b275caed905c33
moonsunghee <loewy.moon@gmail.com>
2024-11-21 16:38:43 +0900

    000

--- src/test/java/kr/or/shammah/d9/StringClassEx.java
+++ src/test/java/kr/or/shammah/d9/StringClassEx.java
@@ -1,5 +1,10 @@
 package kr.or.shammah.d9;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 public class StringClassEx {
     @Test
@@ -22,14 +27,58 @@ public class StringClassEx {
         //----
         System.out.println("str.indexOf('o'), str의 특정문자열의 인덱스 번호를 반환 : "+ str.indexOf("o"));
         //----
-        System.out.println("str.startsWith(),"+str.startsWith("H"));
+        System.out.println("str.startsWith('H'), 특정 문자열로 시작하는지 검사 (t/f): "+str.startsWith("H"));
         //----
-        System.out.println("str.endsWith(), "+str.endsWith("d"));
+        System.out.println("str.endsWith('d'), 특정 문자열로 마치는지 검사 (t/f):  "+str.endsWith("d"));
         //----
         System.out.println("str.repeat(4), str의 문자열을 해당 횟수 만큼 반복 반환 : " + str.repeat(4));
         //----
         System.out.println("str.concat(), str에 문자열을 추가하여 반환 " + str.concat(", My World"));
     }
 
+    @Test
+    void stringGumsa(){
+        String uri = "http://www.google.com/search?q=java";
+        String[] uriArr = uri.split("//");
+        String[] uriArr2 = uriArr[1].split("/");
+        System.out.println(uriArr2[0]);
+    }
+    @Test
+    void arrayCopy(){
+        ArrayList<String> strs = new ArrayList<>();
+        strs.add("김씨");
+        strs.get(0);
+        strs.set(0, "박씨");
+        strs.remove(0);
+    }
 
+    @Test
+    void averPoint(){
+        //--- Cmd+Alt+V : Refactor / Introduce Valuable
+        String point = "80, 60, 70, 100, 90";
+        String[] pt = point.split(", ");
+        List<Integer> ptList = Arrays.stream(pt).map(s -> Integer.parseInt(s)).toList();
+
+        int sum= 0;
+        for (int i = 0; i < ptList.size(); i++) {
+            sum += ptList.get(i);
+        }
+        sum = sum/ptList.size();
+        System.out.println(sum);
+
+        List<Integer> highPt = new ArrayList<>();
+        List<Integer> lowPt = new ArrayList<>();
+
+        for (int i = 0; i < ptList.size(); i++) {
+
+            if(ptList.get(i) > sum){
+                highPt.set(i,  ptList.get(i));
+                System.out.println(highPt);
+            }else{
+                lowPt.set(i,  ptList.get(i));
+                System.out.println(lowPt);
+            }
+
+        }
+    }
 }
