e880efa1182da3e7efe502a29fb9e742744ea89c
moonsunghee <loewy.moon@gmail.com>
2024-11-25 14:38:43 +0900

    ....

--- src/test/java/kr/or/shammah/d9/StringClassEx.java
+++ src/test/java/kr/or/shammah/d9/StringClassEx.java
@@ -64,21 +64,104 @@ public class StringClassEx {
             sum += ptList.get(i);
         }
         sum = sum/ptList.size();
-        System.out.println(sum);
+        System.out.println(sum); //80
 
         List<Integer> highPt = new ArrayList<>();
         List<Integer> lowPt = new ArrayList<>();
 
         for (int i = 0; i < ptList.size(); i++) {
-
             if(ptList.get(i) > sum){
-                highPt.set(i,  ptList.get(i));
-                System.out.println(highPt);
+                highPt.add( ptList.get(i));
+                System.out.println(highPt+"평균위");
             }else{
-                lowPt.set(i,  ptList.get(i));
-                System.out.println(lowPt);
+                lowPt.add(ptList.get(i));
+                System.out.println(lowPt+"평균아래");
             }
-
         }
+        System.out.println(highPt+"평균위, "+lowPt+"평균아래");
+    }
+
+
+    @Test
+    void listWithFunction(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+//        assertThat(getSum(List.of(1, 2, 3, 4))).isEqualTo(10);
+//        assertThat(getSum(List.of(7))).isEqualTo(7);
+//        assertThat(getSum(List.of())).isEqualTo(0);
+//        assertThat(getAverage(List.of(1, 2, 3, 4))).isEqualTo(2.5);
+//        assertThat(getAverage(List.of(7))).isEqualTo(7.0);
+//        assertThat(getAverage(List.of())).isEqualTo(0.0);
+//        assertThat(findMax(List.of(3, 10, 5, 4, 7))).isEqualTo(10);
+//        assertThat(findMax(List.of(7))).isEqualTo(7);
+//        assertThat(isPass(List.of(60, 60))).isTrue();
+//        assertThat(isPass(List.of(40, 80))).isTrue();
+//        assertThat(isPass(List.of( 100, 100, 39))).isFalse();
+//        assertThat(isPass(List.of( 39, 100, 100))).isFalse();
+//        assertThat(isPass(List.of(100, 0, 100))).isFalse();
+        //---------
+        assertThat(getIntegersBetween(1, 5)).isEqualTo(List.of(1, 2, 3, 4, 5));
+        assertThat(getIntegersBetween(-3, 1)).isEqualTo(List.of(-3, -2, -1, 0, 1));
+        assertThat(getIntegersBetween(3, 3)).isEqualTo(List.of(3));
+        assertThat(getIntegersBetween(5, 1)).containsExactlyInAnyOrder(5, 4, 3, 2, 1);
+    }
+    int getSum(List<Integer> numList) {
+        int sum = 0;
+        for(int i=0; i<numList.size(); i++){
+            sum = sum+numList.get(i);
+        }
+        return sum;
+    }
+    double getAverage(List<Integer> numList){
+        double average=0;
+        if(numList.isEmpty()) {
+            average = 0.0;
+        }else{
+            average = (double) getSum(numList) / numList.size();
+        }
+        return average;
+    }
+    int findMax(List<Integer> numList){
+        int maxNum = numList.get(0);
+        for(int i=0; i<numList.size(); i++){
+            maxNum = Math.max(maxNum, numList.get(i));
+        }
+        return maxNum;
+    }
+
+    boolean isPass(List<Integer> numList){
+        int sum = 0;
+        double average = 0;
+
+        boolean subjectFail = true;
+        boolean passOrFail = false;
+
+        for (Integer i : numList) {
+            if(i < 40){
+                subjectFail = false;
+            }
+            sum = sum + i;
+        }
+
+        average = (double) sum/numList.size();
+        if(average >= 60){
+            passOrFail = true;
+        }
+
+        return subjectFail && passOrFail;
+    }
+
+    List<Integer> getIntegersBetween(int start, int end){
+        List<Integer> result = new ArrayList<>();
+
+        if(start <= end){
+            for(int i = start; i <= end; i++){
+                result.add(i);
+            }
+        }else{
+            for(int i = start; i >= end; i--){
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
